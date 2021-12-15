fa45c2c8fdbdce410742f32b36b61eba4a169edc
Óðinn Dagur Bjarnason <36035737+odinndagur@users.noreply.github.com>
2021-12-15 13:02:45 +0000

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <!-- <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link> -->
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+   <script>
+
+    export default {
+     name: 'app',
+     components:{
+     }
+    }
+   </script>
+
+<style >
+
+@supports(padding:max(0px)) {
+    body, header, footer {
+        padding-left: min(0vmin, env(safe-area-inset-left));
+        padding-right: min(0vmin, env(safe-area-inset-right));
+    }
+}
+body{
+    background:#14181c;
+
+}
+html{
+  /* min-height: calc(100% + env(safe-area-inset-top));
+  padding: env(safe-area-inset-top) env(safe-area-inset-right) env(safe-area-inset-bottom) env(safe-area-inset-left); */
+    min-height: calc(100% + env(safe-area-inset-top));
+  padding: env(safe-area-inset-top) env(safe-area-inset-right) env(safe-area-inset-bottom) env(safe-area-inset-left);
+  max-width: 600px;
+  /* height: 100%; */
+  box-sizing: border-box;
+  margin: auto;
+  text-align: center;
+  /* border: 3px solid black; */
+  background:#14181c;
+  color: lightgray;
+  font-family: Georgia, 'Times New Roman', Times, serif;
+}
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="head">
+        <h1>{{greeting}}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            greeting: 'hello',
+        }
+    }
+}
+</script>
+
+<style scoped>
+.head{
+background-color:#ffc90f;
+height:100%;
+/* width:100vw;
+padding:0;
+margin:0;
+box-sizing: border-box; */
+}
+
+/* header{
+  width:100%;
+  height:80;
+  color:#d32228;
+}
+
+.header {
+  display:inline-block;
+  text-align:center;
+    background-color:#ffc90f;
+
+}
+
+.header img {
+  float:left;
+  margin-left: 20px;
+  margin-top:23px;
+} */
+</style>
